df77490c5f941e522be839dce60f0fe4c8a87cda
João Victor <joao151576@gmail.com>
2025-08-02 15:48:02 -0300

    project complete

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: 'https://ocr-caseoutline-backend-production.up.railway.app',
+    origin: [
+      'https://ocr-caseoutline-backend-production.up.railway.app',
+      'https://ocr-caseoutline-backend-production.up.railway.app',
+    ],
     credentials: true,
   });
 
